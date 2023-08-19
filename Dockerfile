9ac604f72f9f8eae2b52c2202355beb86b4126ab
enzbdn <enzo.bodin@supdevinci-edu.fr>
2023-08-19 18:43:33 +0000

    online speech recognition

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,4 @@
 FROM python:3.10.11-slim
-RUN apt-get update \
-    && apt-get install -y --no-install-recommends \
-    ffmpeg
-
 WORKDIR /app
 COPY . /app
 
--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-import logging
-import os
-from typing import BinaryIO
-import ffmpeg
-import numpy as np
-import telebot
-from faster_whisper import WhisperModel
-import time
+import logging, os, telebot, time
+import speech_recognition as sr
+from pydub import AudioSegment
 
 bot_id = os.getenv("BOT_ID")
-model_name = os.getenv("ASR_MODEL", "small")
 allowed_users = os.environ['ALLOWED_USERS']
-precision = os.getenv("PRECISION", "int8")
 
 bot = telebot.TeleBot(bot_id)
-model = WhisperModel(model_name, device="cpu", compute_type=precision)
-SAMPLE_RATE = 16000
 
 @bot.message_handler(
     content_types=["voice"],
@@ -25,29 +16,21 @@ def transcribe_voice_message(message):
     u_id=message.chat.username
     if u_id in allowed_users:
         voice_meta = bot.get_file(message.voice.file_id)
-        voice_audio = load_audio(bot.download_file(voice_meta.file_path))
-        segments, _ = model.transcribe(audio=voice_audio, vad_filter=True)
+        voice_audio = bot.download_file(voice_meta.file_path)
+        with open('audiovoice.ogg', 'wb') as f:
+            f.write(voice_audio)
+        audio = AudioSegment.from_file("audiovoice.ogg", format='ogg')
+        audio.export('audio.wav', format='wav')
+        with sr.AudioFile('audio.wav') as source:
+            audio_data = sr.Recognizer().record(source)
+            text = sr.Recognizer().recognize_google(audio_data, language="fr-FR")
         final_time = time.time()
         processing_time = (final_time - start_time)
-        text = "".join([segment.text for segment in segments])
-        result = f"▶️ {text}\n⌛ *Processing time*: {processing_time:.4f} seconds\n🪄 *ASR_Model*: {model_name}"
+        result = f"▶️ {text}\n⌛ *Processing time*: {processing_time:.4f} seconds"
         bot.reply_to(message, result, parse_mode='Markdown')
 
     else:
         bot.reply_to(message, "Vous n'avez pas la permission d'utiliser le bot")
-
-def load_audio(binary_file: BinaryIO, sr: int = SAMPLE_RATE):
-    try:
-        out, _ = (
-            ffmpeg.input("pipe:", threads=0)
-            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
-            .run(cmd="ffmpeg", capture_stdout=True, capture_stderr=True, input=binary_file)
-        )
-    except ffmpeg.Error as e:
-        raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
-
-    return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
-
 
 logging.warning("Start polling")
 bot.infinity_polling(timeout=10, long_polling_timeout=5)
